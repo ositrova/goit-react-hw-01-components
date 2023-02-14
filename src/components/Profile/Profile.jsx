68b7d16276bd08483f571ff6fd6d9ace64c6aa4e
ositrova <110326755+ositrova@users.noreply.github.com>
2023-02-14 15:07:04 +0200

    profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Profile = ({username, tag, location, avatar, stats }) => {
+  return <div class="profile">
+  <div class="description">
+    <img
+      src={avatar}
+      alt={username}
+      class="avatar"
+    />
+    <p class="name">{username}</p>
+    <p class="tag">@{tag}</p>
+    <p class="location">{location}</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">{stats.views}</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  })
+};
+
+Profile.defaultProps = {
+  name: 'User name',
+  tag: '@User tag',
+  location: 'User location',
+  avatar: 'https://www.flaticon.com/svg/static/icons/svg/3784/3784184.svg',
+  stats: {
+    followers: '0',
+    views: '0',
+    likes: '0',
+  },
+};
+
+export default Profile;
